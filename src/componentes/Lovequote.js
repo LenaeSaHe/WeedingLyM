b7c137e5f7050c02f6289b0ef7e81aac6ceee81a
Lena <lena_sh@outlook.com>
2022-12-27 20:21:08 -0600

    Componente Lovequote

---
+++ src/componentes/Lovequote.js
@@ -0,0 +1,23 @@
+function Lovequote()
+{
+    return  <section class="contA">
+                <div class="container">
+                    <div class="content row">
+                        <div class="col-12" id="lovequote">
+                            <div class="lovecard">
+                                <h3 class="card-title text-center txtTitles">Lorem ipsum, dolor sit amet</h3>
+                                <p class="text-center">
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                                    Doloremque, culpa. Iure quis eum ratione animi aperiam neque est
+                                    expedita autem inventore. Doloribus suscipit laboriosam ullam
+                                    fugiat. Impedit praesentium expedita quae?
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+  
+
+}
+export default Lovequote;
