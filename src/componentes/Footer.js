71f9c99ebcab10beedb690385fa9834898ceadc0
Lena <lena_sh@outlook.com>
2022-12-29 21:11:30 -0600

    Componente footer

---
+++ src/componentes/Footer.js
@@ -0,0 +1,13 @@
+
+
+function Footer()
+{
+    return  <section>
+                <div className="col-12 bottom">
+                    <p className="text-center txtText">Por favor, confirmanos tu asistencia <br />
+                        <button type="button" className="btn btn-light">Si, asistiré</button>
+                    </p>
+                </div>
+            </section>
+}
+export default Footer;
