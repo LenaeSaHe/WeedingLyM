6bd0bd53131f244eda8e8acdc605d0b37ac76721
Lena <lena_sh@outlook.com>
2022-12-29 21:12:22 -0600

    Componente Hotel

---
+++ src/componentes/hotel.js
@@ -0,0 +1,13 @@
+function Hotel()
+{
+    return     <section className="contA">
+                    <div className="container">
+                        <div className="row content">
+                            <div className="col-6 txtTitles back">Hospedaje 1</div>
+                            <div className="col-6 txtTitles back">Hospedaje 2</div>
+                        </div>
+                    </div>
+                </section>
+
+}
+export default Hotel;
