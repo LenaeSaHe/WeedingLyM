c26512f52e763eec176d2d50796dada419214f6d
Marcos Rivas <marcosrivasr@gmail.com>
2022-12-30 15:46:45 -0600

    Diseño de countdown

--- src/componentes/Countdown.js
+++ src/componentes/Countdown.js
@@ -1,55 +1,57 @@
 import { useState, useEffect } from "react";
-function Countdown()
-{
+import CountdownNumber from "./CountdownNumber";
+function Countdown() {
   const [days, setDays] = useState(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
 
-  useEffect(()=>
-  {
+  useEffect(() => {
     const DATE_TARGET = new Date("11/04/2023 03:00 PM");
     const MILLISECONDS_OF_A_SECOND = 1000;
     const MILLISECONDS_OF_A_MINUTE = MILLISECONDS_OF_A_SECOND * 60;
     const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
     const MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24;
-    function updateCountdown() 
-    {
+    function updateCountdown() {
       const NOW = new Date();
       const DURATION = DATE_TARGET - NOW;
       const REMAINING_DAYS = Math.floor(DURATION / MILLISECONDS_OF_A_DAY);
-      const REMAINING_HOURS = Math.floor((DURATION % MILLISECONDS_OF_A_DAY) / MILLISECONDS_OF_A_HOUR);
-      const REMAINING_MINUTES = Math.floor((DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE);
-      const REMAINING_SECONDS = Math.floor((DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND);
+      const REMAINING_HOURS = Math.floor(
+        (DURATION % MILLISECONDS_OF_A_DAY) / MILLISECONDS_OF_A_HOUR
+      );
+      const REMAINING_MINUTES = Math.floor(
+        (DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE
+      );
+      const REMAINING_SECONDS = Math.floor(
+        (DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND
+      );
       setDays(REMAINING_DAYS);
       setHours(REMAINING_HOURS);
       setMinutes(REMAINING_MINUTES);
       setSeconds(REMAINING_SECONDS);
     }
     updateCountdown();
-    setInterval(updateCountdown, MILLISECONDS_OF_A_SECOND);  
-  },[])
+    setInterval(updateCountdown, MILLISECONDS_OF_A_SECOND);
+  }, []);
 
-
-    return <section className="contA">
-            <div className="container">
-              <div className="content row">
-                <div className="col-12">
-                  <p className="text-center txtTitles">Ya falta muy poco</p>
-                </div>
-                <div className="col-12">
-                  <p className="text-center txtBadage">
-                    Dias<span className="badge text-bg-secondary text-bg-light" id="days">{days}</span>
-                    Horas<span className="badge text-bg-secondary text-bg-light" id="hours">{hours}</span>
-                    Minutos<span className="badge text-bg-secondary text-bg-light" id="minutes">{minutes}</span>
-                    Segundos<span className="badge text-bg-secondary text-bg-light" id="seconds">{seconds}</span>
-                  </p>
-                </div>
-              </div>
-            </div>
-            </section>
-
-  
-
+  return (
+    <section className="contA" id="countdown">
+      <div className="container">
+        <div className="content row">
+          <div className="col-12">
+            <h2 className="text-center">Ya falta muy poco</h2>
+          </div>
+          <div className="col-12">
+            <p className="text-center txtBadage">
+              <CountdownNumber text="Días" value={days} />
+              <CountdownNumber text="Horas" value={hours} />
+              <CountdownNumber text="Minutos" value={minutes} />
+              <CountdownNumber text="Segundos" value={seconds} />
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 export default Countdown;
--- src/componentes/Giveaway.js
+++ src/componentes/Giveaway.js
@@ -1,52 +1,51 @@
 import liverpool from "../img/liver.png";
 import amazon from "../img/amazon.png";
-function Giveaway() 
-{
-    return     <section className="contA">
-    <div className="container">
-      <div className="row content">
+function Giveaway() {
+  return (
+    <section className="contA">
+      <div className="container">
+        <div className="row content">
+          <div className="col-12 back" id="divtable">
+            <h2>Mesa de regalos</h2>
+            <p className="text-center txtText">
+              ¡ Valoramos mucho tu asistencia !<br />
+              Si deseas tener algún detalle con nosotros, te dejamos algunas
+              sugerencias
+            </p>
+          </div>
 
-        <div className="col-12 back" id="divtable">
-          <h1 className="text-center txtTitles">Mesa de regalos</h1>
-          <p className="text-center txtText">
-            ¡ Valoramos mucho tu asistencia !<br />
-            Si deseas tener algún detalle con nosotros, te dejamos algunas
-            sugerencias
-          </p>
-        </div>
-
-        <div className="col-12 backable back">
-          <div className="card center boxStore anyborder">
-            <div className="card-body anyborder backR">
-              <h5 className="card-title text-center txtTitles">Liverpool</h5>
-              <p className="card-text text-center anyborder">
-                <img src={liverpool} className="card-img-top brand" />
-              </p>
-              <p className="text-center">
-                <a href="#" className="btn btn-primary text-center"
-                  >Ver mesa de regalos</a>
-              </p>
+          <div className="col-12 backable back">
+            <div className="card center boxStore anyborder">
+              <div className="card-body anyborder backR">
+                <h5 className="card-title text-center txtTitles">Liverpool</h5>
+                <p className="card-text text-center anyborder">
+                  <img src={liverpool} className="card-img-top brand" />
+                </p>
+                <p className="text-center">
+                  <a href="#" className="btn btn-primary text-center">
+                    Ver mesa de regalos
+                  </a>
+                </p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="card center boxStore anyborder">
+          <div className="card center boxStore anyborder">
             <div className="card-body anyborder backR">
               <h5 className="card-title text-center txtTitles">Amazon</h5>
               <p className="card-text text-center anyborder">
-                <img src={amazon} className="card-img-top brand"/>
+                <img src={amazon} className="card-img-top brand" />
               </p>
               <p className="text-center">
-                <a href="#" className="btn btn-primary text-center"
-                  >Ver mesa de regalos</a>
+                <a href="#" className="btn btn-primary text-center">
+                  Ver mesa de regalos
+                </a>
               </p>
             </div>
           </div>
-
+        </div>
       </div>
-    </div>
-  </section>
-
-
+    </section>
+  );
 }
 export default Giveaway;
