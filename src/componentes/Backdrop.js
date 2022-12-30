c4daf9f22a6814e8bcbace4a98667cf3003cb644
Lena <lena_sh@outlook.com>
2022-12-29 21:11:16 -0600

    Componente backdrop

---
+++ src/componentes/Backdrop.js
@@ -0,0 +1,9 @@
+import logo from "../img/mono.gif"
+function Backdrop()
+{    
+    return  <div id="backdrop">
+                <img src={logo} className="center"/>
+            </div>
+
+}
+export default Backdrop;
