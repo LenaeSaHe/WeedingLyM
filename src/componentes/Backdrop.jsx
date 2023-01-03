8807dfeb9b3cc04982663b1b4ce3bd9c112c4548
Lena <lena_sh@outlook.com>
2023-01-02 20:50:58 -0600

    Tamaño fallido del backdrop

--- src/componentes/Backdrop.jsx
+++ src/componentes/Backdrop.jsx
@@ -1,13 +1,26 @@
+import { useEffect, useRef } from "react";
 import logo from "../img/mono.gif";
-function Backdrop({ invitado }) {
+function Backdrop({ invitado }) 
+{
+  useEffect (function(){
+    addScroll();
+  }, [])
+  const ref = useRef(); {/* obtener la referencia del html*/}
+  function addScroll()
+  {
+    ref.current.addEventListener("animationend", function()
+    {
+      document.querySelector("body").style.overflow ="auto";
+    })
+  }
   return (
-    <div id="backdrop">
+    <div id="backdrop" ref={ref}>
       <div>
         <span className="title fadeIn delay-5">
           ¡Hola, {invitado ? invitado.nombre.split(" ")[0] : ""}!
         </span>
       </div>
-      <img src={logo} />
+      <img className="logo" src={logo} />
       <div className="message w-50 text-center fadeIn delay-5">
         Te invitamos a compartir este bello momento con nosotros
       </div>
