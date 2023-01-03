e463c85a1962518cfe906187e81a82a3758e2652
Marcos Rivas <marcosrivasr@gmail.com>
2023-01-02 21:35:22 -0600

    cambios a backdrop

--- src/componentes/Backdrop.jsx
+++ src/componentes/Backdrop.jsx
@@ -1,28 +1,30 @@
 import { useEffect, useRef } from "react";
 import logo from "../img/mono.gif";
-function Backdrop({ invitado }) 
-{
-  useEffect (function(){
+function Backdrop({ invitado }) {
+  useEffect(function () {
     addScroll();
-  }, [])
-  const ref = useRef(); {/* obtener la referencia del html*/}
-  function addScroll()
+  }, []);
+  const ref = useRef();
   {
-    ref.current.addEventListener("animationend", function()
-    {
-      document.querySelector("body").style.overflow ="auto";
-    })
+    /* obtener la referencia del html*/
+  }
+  function addScroll() {
+    ref.current.addEventListener("animationend", function () {
+      document.querySelector("body").style.overflow = "auto";
+    });
   }
   return (
     <div id="backdrop" ref={ref}>
-      <div>
-        <span className="title fadeIn delay-5">
-          ¡Hola, {invitado ? invitado.nombre.split(" ")[0] : ""}!
-        </span>
-      </div>
-      <img className="logo" src={logo} />
-      <div className="message w-50 text-center fadeIn delay-5">
-        Te invitamos a compartir este bello momento con nosotros
+      <div className="backdropContainer">
+        <div>
+          <span className="title fadeIn delay-5">
+            ¡Hola, {invitado ? invitado.nombre.split(" ")[0] : ""}!
+          </span>
+        </div>
+        <img className="logo" src={logo} />
+        <div className="message w-50 m-auto fadeIn delay-5">
+          Te invitamos a compartir este bello momento con nosotros
+        </div>
       </div>
     </div>
   );
