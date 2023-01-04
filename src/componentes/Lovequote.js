d47c78b2039294c4af180ea95ee3b3384ef29379
Marcos Rivas <marcosrivasr@gmail.com>
2023-01-03 23:48:28 -0600

    frase romántica

--- src/componentes/Lovequote.js
+++ src/componentes/Lovequote.js
@@ -1,23 +1,24 @@
-function Lovequote()
-{
-    return  <section class="contA">
-                <div class="container">
-                    <div class="content row">
-                        <div class="col-12" id="lovequote">
-                            <div class="lovecard">
-                                <h3 class="card-title text-center txtTitles">Lorem ipsum, dolor sit amet</h3>
-                                <p class="text-center txtParagraph widthLovequote m-auto mt-3 container-sm">
-                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                                    Doloremque, culpa. Iure quis eum ratione animi aperiam neque est
-                                    expedita autem inventore. Doloribus suscipit laboriosam ullam
-                                    fugiat. Impedit praesentium expedita quae?
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-  
-
+function Lovequote() {
+  return (
+    <section class="contA">
+      <div class="container">
+        <div class="content row">
+          <div class="col-12" id="lovequote">
+            <div class="lovecard">
+              <h3 class="card-title text-center txtTitles">
+                Por siempre el amor de mi vida
+              </h3>
+              <p class="text-center txtParagraph widthLovequote m-auto mt-3 container-sm">
+                Eres el amor de mi vida y hoy ya no puedo imaginarme un día sin
+                ti. Tomémonos la mano como la primera vez y caminemos juntos
+                como el mejor equipo que somos, con mucho amor y respeto por
+                siempre mi amor.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 export default Lovequote;
