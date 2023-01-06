339774731db048d1ee6614b621168ab0306538de
Lena <lena_sh@outlook.com>
2023-01-05 23:07:43 -0600

    Sección de save the date

--- src/componentes/Savethedate.js
+++ src/componentes/Savethedate.js
@@ -1,10 +1,20 @@
 function Savethedate()
 {
-    return  <section class="contB">
-                <div class="container">
-                    <div class="content row">
-                        <div class="col-12 save">Save the date</div>
+    return  <section class="contB" id="secDate">
+                <div class="cristalSave" id="contDate">
+                    <div class="txtTitles"> 
+                        Save the date
                     </div>
+                    <p class="txtParagraph w-50">
+                        Eres muy importante para nostros, y en este día 
+                        tan especial en el que frente a Dios y a nuestros seres amados
+                        recibiremos la bendición de un nuevo capítulo en nuestras vidas,
+                        deseamos nos acompañes el próximo
+                    </p>
+                    <div class="txtTitles"> 
+                        <p><b>04.Nov.23</b></p>
+                    </div>
+                    <small class="hrSave txtParagraph">14:00 hrs.</small>
                 </div>
             </section>
 }
