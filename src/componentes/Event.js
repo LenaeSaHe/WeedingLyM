560e4193ed52016c56148f96e52b03a8f50757e8
Marcos Rivas <marcosrivasr@gmail.com>
2023-01-08 17:44:37 -0600

    Responsive de timeline

--- src/componentes/Event.js
+++ src/componentes/Event.js
@@ -1,7 +1,14 @@
-function Event({imagen, titulo})
-{
-    return <div>
-                <time><img src={imagen} className="center" /> {titulo} </time>
-            </div>
+function Event({ imagen, titulo, time }) {
+  return (
+    <div className="eventContainer">
+      <div className="icon">
+        <img src={imagen} className="center" />
+      </div>
+      <div className="eventInfo">
+        <h3>{titulo}</h3>
+        <time>{time}</time>
+      </div>
+    </div>
+  );
 }
 export default Event;
