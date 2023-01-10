dc0f425ccd10be2221b46f072e16177a30d3d86e
Marcos Rivas <marcosrivasr@gmail.com>
2023-01-10 00:44:03 -0600

    Seccion family

--- src/componentes/Family.js
+++ src/componentes/Family.js
@@ -1,31 +1,54 @@
+import photoFamF from "../img/papas.jpg";
+import photoFamM from "../img/papas.jpg";
+function Family() {
+  return (
+    <section className="contB">
+      <div className="container">
+        <div className="content row">
+          <div className="col-lg-6 col-sm-12 center pb-5">
+            <div className="row">
+              <div className="col">
+                <p className="txtTitles2">Padres de la novia</p>
+              </div>
+            </div>
 
-import photoFamF from "../img/papas.jpg"
-import photoFamM from "../img/papas.jpg"
-function Family()
-{
-  
-    return <section className="contB">
-    <div className="container">
-      
-      <div className="content row">
-        <div className="col-6 center txtTitles2">Padres de la novia</div>
-        <div className="col-6 center txtTitles2">Padres del novio</div>
+            <div className="row">
+              <div className="col center py-3">
+                <img src={photoFamF} className="img-fluid photofam" />
+              </div>
+            </div>
 
-        <div className="col-6 center py-3"><img src={photoFamF} className="img-fluid photofam" /></div>
-        <div className="col-6 center py-3"><img src={photoFamF} className="img-fluid photofam" /></div>
+            <div className="row">
+              <div className="col center txtTitles4 fs-3">
+                - Julio César Sánchez Rivera
+                <br />- Maria Elena Hernández Hernández
+              </div>
+            </div>
+          </div>
 
-        <div className="col-6 center txtTitles2">
-          - Julio César Sánchez Rivera<br/>- Maria Elena Hernández Hernández
+          <div className="col-lg-6 col-sm-12 center txtTitles2 pb-5">
+            <div className="row">
+              <div className="col">
+                <h2 className="txtTitles2">Padres del novio</h2>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col center py-3">
+                <img src={photoFamF} className="img-fluid photofam" />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col center txtTitles4 fs-3">
+                - Marcos Rivas Arzaluz
+                <br />- Alejandra Rojas Brito
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="col-6 center txtTitles2">
-          - Marcos Rivas Arzaluz<br/>- Alejandra Rojas Brito
-        </div>
-        
       </div>
-
-
-      </div>
-  </section>
-
+    </section>
+  );
 }
 export default Family;
