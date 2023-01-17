4badcd5ed940825191bbcf903aeb83d614c1f7c4
Lena <lena_sh@outlook.com>
2023-01-17 00:17:17 -0600

    Google calendar en save the date

--- src/componentes/Savethedate.js
+++ src/componentes/Savethedate.js
@@ -1,20 +1,31 @@
+import googleCalendar from "../img/calendar_google.png";
+import googleicon from "../img/google.png";
 function Savethedate({ invitado }) {
   return (
-    <section class="contB" id="secDate">
-      <div class="cristalSave" id="contDate">
-        <div class="txtTitles">Save the date</div>
-        <p class="txtParagraph w-50">
+    <section className="contB" id="secDate">
+      <div className="cristalSave" id="contDate">
+        <div className="txtTitles">Save the date</div>
+        <p className="txtParagraph w-50">
           {invitado ? invitado.nickname : ""}, eres muy importante para nostros,
           y en este día tan especial en el que frente a Dios y a nuestros seres
           amados recibiremos la bendición de un nuevo capítulo en nuestras
           vidas, deseamos nos acompañes el próximo
         </p>
-        <div class="txtTitles">
+        <div className="txtTitles">
           <p>
             <b>04.Nov.23</b>
           </p>
         </div>
-        <small class="hrSave txtParagraph">14:00 hrs.</small>
+        <small className="hrSave txtParagraph">14:00 hrs.</small>
+        <a href="https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=MmVhaG5kOTJzZmhjbG1qa2VzcWVoZmg1djQgbGl6enkuc2FoZUBt&tmsrc=lizzy.sahe%40gmail.com" target="_blank">
+          <img src={googleCalendar}/>
+        </a>
+        <small className="txtParagraph">
+          <a href="https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=MmVhaG5kOTJzZmhjbG1qa2VzcWVoZmg1djQgbGl6enkuc2FoZUBt&tmsrc=lizzy.sahe%40gmail.com" target="_blank">
+            <strong>Agrega el evento a tu calendario en </strong>&nbsp;
+            <img src={googleicon} width="30"/>
+          </a>
+        </small>
       </div>
     </section>
   );
