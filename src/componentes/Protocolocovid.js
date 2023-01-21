f3ff00fd64a0134b5d3884f55afab30ac8dd7cd7
Lena <lena_sh@outlook.com>
2023-01-21 00:17:04 -0600

    tamaño del texto en protocolo

--- src/componentes/Protocolocovid.js
+++ src/componentes/Protocolocovid.js
@@ -1,17 +1,15 @@
 function Protocolocovid()
 {
-    return <footer>
-                <p><h1 className="center txtTitles">Protocolo de seguridad e higiene</h1></p>
+    return <footer className="smalldiv">
+                <h1 className="center smallTitle">Protocolo de seguridad e higiene</h1>
                     <div className="d-flex justify-content-center align-items-center">
-                        <p>
-                            <ul>
-                                <li>Por favor respeta las medidas de sanidad durante el evento</li>
-                                <li>No olvides continuar con el uso de cubrebocas y gel antibacteríal </li>
-                                <li>Evita el contacto de tus manos con nariz y boca</li>
-                                <li> Mantén la sana distancia </li>
-                                <li>Si presentas algún síntoma previo al evento, háznoslo saber, comprenderemos tu ausencia.</li>
-                            </ul>
-                        </p>
+                        <ul className="smallletter">
+                            <li>Por favor respeta las medidas de sanidad durante el evento</li>
+                            <li>No olvides continuar con el uso de cubrebocas y gel antibacteríal </li>
+                            <li>Evita el contacto de tus manos con nariz y boca</li>
+                            <li> Mantén la sana distancia </li>
+                            <li>Si presentas algún síntoma previo al evento, háznoslo saber, comprenderemos tu ausencia.</li>
+                       </ul>
                     </div>                
             </footer>
 }
