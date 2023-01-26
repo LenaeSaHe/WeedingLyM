55ad34873a4520194bff48554f9c3cd56b23556f
Lena <lena_sh@outlook.com>
2023-01-25 23:09:21 -0600

    protocolo covid

--- src/componentes/Protocolocovid.js
+++ src/componentes/Protocolocovid.js
@@ -1,16 +1,23 @@
+import Title from "./common/Title";
+import mask from "../img/mask.ico";
+import icocara from "../img/icocara.jpg";
+import doctor from "../img/doctor.ico";
+import distancia from "../img/distancia.jpg";
 function Protocolocovid()
 {
-    return <footer className="smalldiv">
-                <h1 className="center smallTitle">Protocolo de seguridad e higiene</h1>
-                    <div className="d-flex justify-content-center align-items-center">
-                        <ul className="smallletter">
-                            <li>Por favor respeta las medidas de sanidad durante el evento</li>
-                            <li>No olvides continuar con el uso de cubrebocas y gel antibacteríal </li>
-                            <li>Evita el contacto de tus manos con nariz y boca</li>
-                            <li> Mantén la sana distancia </li>
-                            <li className="fontcovid"><b>Si presentas algún síntoma previo al evento, háznoslo saber, comprenderemos tu ausencia.</b></li>
-                       </ul>
+    return <section className="smalldiv">
+                <Title>Protocolo de seguridad e higiene durante el evento</Title>
+                    <div className="d-flex flex-column justify-content-center align-items-center container">
+                        <div className="row mt-5">
+                            <div className="col-3 text-center smallletter"><img src={doctor} width="48"/><br/>Por favor, respeta las medidas de sanidad.</div>
+                            <div className="col-3 text-center smallletter"><img src={mask} width="48"/><br/>Continua con el uso de cubrebocas y gel antibacteríal.</div>
+                            <div className="col-3 text-center smallletter"><img src={icocara} width="48"/><br/>Evita el contacto de tus manos con nariz y boca.</div>
+                            <div className="col-3 text-center smallletter"><img src={distancia} width="70"/><br/>Mantén la sana distancia.</div>
+                        </div>
+                        <div className="row mt-3">
+                            <p className="fontcovid"><b>Si presentas algún síntoma previo al evento, háznoslo saber, comprenderemos tu ausencia.</b></p>
+                        </div>
                     </div>                
-            </footer>
+            </section>
 }
 export default Protocolocovid;
