3d6fddedd1692a7c5a51988b865614b92315bbbb
Marcos Rivas <marcosrivasr@gmail.com>
2023-02-16 14:24:30 -0600

    header improved

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,43 +1,22 @@
 import { useRef } from "react";
 import { useEffect } from "react";
 import monograma from "../img/Imagen2.png";
+import cover from "../img/mainLyM.jpg";
 import CarrouselHeader from "./CarrouselHeader";
 
 function Header({ invitado }) {
-  const ref = useRef(null);
-  const refHeader = useRef(null);
-  useEffect(() => {
-    window.addEventListener("scroll", function () {
-      const offset = window.scrollY;
-      if (ref.current) {
-        ref.current.style.top = -offset / 5 + "px";
-      }
-
-      if (refHeader.current) {
-        if (
-          offset >= refHeader.current.offsetTop &&
-          offset <= refHeader.current.offsetHeight - 200
-        ) {
-          refHeader.current.style.backgroundSize = 100 + offset / 20 + "%";
-          //refHeader.current.style.height = 100 + offset / 20 + "vh";
-        }
-        console.log(
-          offset,
-          refHeader.current.offsetTop,
-          refHeader.current.offsetHeight
-        );
-      }
-      //console.log(offset / 5 + "%");
-    });
-  }, []);
   return (
-    <header id="" ref={refHeader}>
-      <div className="content" ref={ref}>
-        <div className="fontSize45 text-shadow">¡Nos casamos!</div>
-        <div className="fontSize90 font-bold text-shadow">Lena & Marcos</div>
-        <div className="fontSize30 text-shadow">
-          {invitado?.nickname},<br />
-          te invitamos a formar parte de este momento
+    <header>
+      <div className="background">
+        <div className="content">
+          <div className="fontSize45 text-shadow">¡Nos casamos!</div>
+          <div className="fontSize90 font-bold text-shadow front">
+            Lena & Marcos
+          </div>
+          <div className="fontSize30 text-shadow fadeIn back ">
+            {invitado?.nickname},<br />
+            te invitamos a formar parte de este momento
+          </div>
         </div>
       </div>
     </header>
