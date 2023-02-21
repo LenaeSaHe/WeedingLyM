a14fe2c740c3ffd3b0739efd047c12ff366d1869
Marcos Rivas <marcosrivasr@gmail.com>
2023-02-20 21:27:25 -0600

    family section

--- src/componentes/Family.js
+++ src/componentes/Family.js
@@ -1,52 +1,22 @@
-import photoFamF from "../img/papas.jpg";
 import photoFamM from "../img/papas.jpg";
+import flowersCircle from "../img/circulo-flores.png";
 import Title from "./common/Title";
+import FamilyItem from "./FamilyItem";
 function Family() {
   return (
-    <section className="contB">
+    <section className="contB familyContainer">
       <div className="container">
         <div className="content row">
-          <div className="col-lg-6 col-sm-12 center pb-5">
-            <div className="row">
-              <div className="col">
-                <Title>Padres de la novia</Title>
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="col center py-3">
-                <img src={photoFamF} className="img-fluid photofam" />
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="col center txtTitles fs-3">
-                - Julio César Sánchez Rivera
-                <br />- Maria Elena Hernández Hernández
-              </div>
-            </div>
-          </div>
-
-          <div className="col-lg-6 col-sm-12 center pb-5">
-            <div className="row">
-              <div className="col">
-              <Title>Padres del novio</Title>
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="col center py-3">
-                <img src={photoFamF} className="img-fluid photofam" />
-              </div>
-            </div>
-
-            <div className="row">
-              <div className="col center txtTitles fs-3">
-                - Marcos Rivas Arzaluz
-                <br />- Alejandra Rojas Brito
-              </div>
-            </div>
-          </div>
+          <FamilyItem
+            title="Padres de la novia"
+            father="Julio César Sánchez Rivera"
+            mother="Maria Elena Hernández Hernández"
+          />
+          <FamilyItem
+            title="Padres del novio"
+            father="Marcos Rivas Arzaluz"
+            mother="Alejandra Rojas Brito"
+          />
         </div>
       </div>
     </section>
---
+++ src/componentes/FamilyItem.jsx
@@ -0,0 +1,69 @@
+import Title from "./common/Title";
+import { useRef, useState, useEffect } from "react";
+import classNames from "classnames";
+
+let options = {
+  root: null,
+  rootMargin: "20px",
+  threshold: 0.4,
+};
+
+export default function FamilyItem({ title, father, mother }) {
+  const ref = useRef(null);
+  const [show, setShow] = useState(false);
+  const callbackFunc = (entries) => {
+    entries.forEach((entry) => {
+      setShow(entry.isIntersecting);
+      if (entry.isIntersecting) {
+      }
+    });
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(callbackFunc, options);
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      observer.unobserve(ref.current);
+    };
+  }, []);
+  return (
+    <div className="col-lg-6 col-sm-12 center pb-5" ref={ref}>
+      <div className="row">
+        <div className="col">
+          <Title separator={false}>{title}</Title>
+        </div>
+      </div>
+
+      <div className="row">
+        <div
+          className={classNames(
+            "col",
+            "center",
+            "photofam",
+            "famPhotos",
+            "photoPapasLena",
+            show ? "showFam" : ""
+          )}
+        ></div>
+      </div>
+
+      <div className="row">
+        <div
+          className={classNames(
+            "col",
+            "center",
+            "famNames",
+            show ? "showFam" : ""
+          )}
+        >
+          {father}
+          <br />
+          {mother}
+        </div>
+      </div>
+    </div>
+  );
+}
