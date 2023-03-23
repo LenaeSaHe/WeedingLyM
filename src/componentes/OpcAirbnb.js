208e21ccb527cb4c2385187679673a01e4059880
Lena <lena_sh@outlook.com>
2023-03-22 23:26:26 -0600

    Hospedaje

--- src/componentes/OpcAirbnb.js
+++ src/componentes/OpcAirbnb.js
@@ -1,20 +1,13 @@
 function OpcAirbnb({ titulo, image, linkweb, logo }) {
   return (
     <div className=" col-12 col-lg-4 txtTitles d-flex align-items-center justify-content-center">
-      <div className="card" style={{ width: "25rem", marginBottom:"10px", height:"450px"}}>
+      <div className="card" style={{ width: "25rem", marginBottom:"10px", height:"400px"}}>
         <img src={image} className="card-img-top" />
         <div className="card-body">
           <h5 className="txtCardHotel center">{titulo}</h5>
-          <div className="d-flex justify-content-center">
-            <img
-              src={logo}
-              className=""
-              style={{ width: "5rem" }}
-            />
-          </div>
           <div className="txtCardHotel center">
             <a href={linkweb} className="btn btn-primary" target="_blank">
-              Reservar
+              Reservar airbnb <img src={logo} width="32"/>
             </a>
           </div>
         </div>
--- src/componentes/hotel.js
+++ src/componentes/hotel.js
@@ -1,9 +1,12 @@
 import coralhotel from "../img/hotelcoral.webp";
 import villas from "../img/villashotel.jpg";
+import holidayinn from "../img/holidayinn.jpg";
+import fiestaamericana from "../img/fiestaamericana.jpg";
 
-import airbnbLogo from "../img/airbnb-logo.png";
+import airbnbLogo2 from "../img/airbnb-logo.png";
 import airbnbA from "../img/airbnb1.webp";
 import airbnbB from "../img/airbnb2.webp";
+import airbnbLogo from "../img/circleairbnb.png";
 
 import OpcHotel from "./OpcHotel";
 import OpcAirbnb from "./OpcAirbnb";
@@ -16,6 +19,12 @@ const hotel1Web = "https://club.coralcuernavaca.com.mx/";
 
 const hotel2 = "https://www.hotellasbrisasdecuernavaca.com/page17.html";
 const hotel2Web = "https://www.hotellasbrisasdecuernavaca.com/";
+
+const hotel3 = "https://www.ihg.com/holidayinn/hotels/us/en/find-hotels/select-roomrate?fromRedirect=true&qSrt=sBR&glat=FREE_hpa_free_MX_desktop_CVJMX_mapresults_1_MXN_2023-03-23_default___FALSE_CSTNIGCOR&qSlH=CVJMX&qRms=1&qAdlt=2&qChld=0&qCiD=23&qCiMy=022023&qCoD=24&qCoMy=022023&qrtPt=1650.00&setPMCookies=true&qSlRc=CSTN&qRtP=IGCOR&qSHBrC=HI&qDest=Blvd.%20Gustavo%20Diaz%20Ordaz%20%2386,%20Cuernavaca,%20MOR,%20MX&cm_mmc=hpa_free_MX_desktop_CVJMX_mapresults_1_MXN_2023-03-23_default___FALSE_CSTNIGCOR&srb_u=1";
+const hotel3Web="https://www.ihg.com/holidayinn/hotels/us/en/find-hotels/select-roomrate?fromRedirect=true&qSrt=sBR&glat=FREE_hpa_free_MX_desktop_CVJMX_mapresults_1_MXN_2023-03-23_default___FALSE_CSTNIGCOR&qSlH=CVJMX&qRms=1&qAdlt=2&qChld=0&qCiD=23&qCiMy=022023&qCoD=24&qCoMy=022023&qrtPt=1650.00&setPMCookies=true&qSlRc=CSTN&qRtP=IGCOR&qSHBrC=HI&qDest=Blvd.%20Gustavo%20Diaz%20Ordaz%20%2386,%20Cuernavaca,%20MOR,%20MX&cm_mmc=hpa_free_MX_desktop_CVJMX_mapresults_1_MXN_2023-03-23_default___FALSE_CSTNIGCOR&srb_u=1";
+
+const hotel4 = "https://reservations.fiestamericana.com/97959?_ga=2.95200287.282289134.1679547872-1127338853.1679547872&adults=2&datein=11/03/2023&dateout=11/05/2023&languageid=2#/accommodation/room";
+const hotel4web = "https://www.fiestamericana.com/hoteles-y-resorts/fiesta-americana-hacienda-san-antonio-el-puente-cuernavaca";
 
 const airbnb1 =
   "https://www.airbnb.mx/rooms/45847721?check_in=2023-02-10&check_out=2023-02-12&guests=1&adults=1&s=67&unique_share_id=3981691f-9592-4609-9ac6-24275d0b457c";
@@ -47,10 +56,10 @@ function Hotel() {
           />
 
           <OpcHotel
-            titulo="Villas Cuernavaca Hotel"
-            image={villas}
-            linkweb={hotel2Web}
-            link={hotel2}
+            titulo="Holiday Inn"
+            image={holidayinn}
+            linkweb={hotel3Web}
+            link={hotel3}
           />
           
           <OpcAirbnb
@@ -66,12 +75,12 @@ function Hotel() {
             logo={airbnbLogo}
           />
 
-          <OpcAirbnb
-            titulo="Casita Rústica"
-            image={airbnbB}
-            linkweb={airbnb2}
-            logo={airbnbLogo}
-          />
+          <OpcHotel
+            titulo="Fiesta Americana"
+            image={fiestaamericana}
+            linkweb={hotel4}
+            link={hotel4web}
+          />  
 
         </div>
       </div>
