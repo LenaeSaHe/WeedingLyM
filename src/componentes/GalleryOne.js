b5c5a5c7bda42c4f694e0a25edf6ba239a74e8c0
Marcos Rivas <marcosrivasr@gmail.com>
2023-03-26 19:24:20 -0600

    Animacion de imagenes

--- src/componentes/GalleryOne.js
+++ src/componentes/GalleryOne.js
@@ -1,22 +1,69 @@
+import { useRef, useState, useEffect } from "react";
+import classNames from "classnames";
 import foto1 from "../img/mainLyM4.jpg";
 
-function GalleryOne() 
-{
-    return (<section className="contA">
-               
-                <div className="row d-flex align-items-center justify-content-center galleryOneContainer">
+let options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.4,
+};
 
-                    <div className="col-lg-4">
-                        <img src={foto1} className="ourPhoto" />
-                    </div>   
-                    <div className="col-lg-4">
-                        <img src={foto1} className="ourPhoto" />
-                    </div>
-                    <div className="col-lg-4">
-                        <img src={foto1} className="ourPhoto" />
-                    </div>
-                </div>
+function GalleryOne() {
+  const ref = useRef(null);
+  const [show, setShow] = useState(false);
+  const callbackFunc = (entries) => {
+    entries.forEach((entry) => {
+      setShow(entry.isIntersecting);
+      if (entry.isIntersecting) {
+      }
+    });
+  };
 
-            </section>);
+  useEffect(() => {
+    const observer = new IntersectionObserver(callbackFunc, options);
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      observer.unobserve(ref.current);
+    };
+  }, []);
+  return (
+    <section className="contA" ref={ref}>
+      <div className="row d-flex align-items-center justify-content-center galleryOneContainer">
+        <div className="col-lg-4">
+          <img
+            src={foto1}
+            className={classNames(
+              "ourPhoto",
+              "delay-01",
+              show ? "showOurPhoto" : ""
+            )}
+          />
+        </div>
+        <div className="col-lg-4">
+          <img
+            src={foto1}
+            className={classNames(
+              "ourPhoto",
+              "delay-02",
+              show ? "showOurPhoto" : ""
+            )}
+          />
+        </div>
+        <div className="col-lg-4">
+          <img
+            src={foto1}
+            className={classNames(
+              "ourPhoto",
+              "delay-03",
+              show ? "showOurPhoto" : ""
+            )}
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
 export default GalleryOne;
--- src/componentes/GalleryTwo.js
+++ src/componentes/GalleryTwo.js
@@ -1,20 +1,61 @@
+import { useRef, useState, useEffect } from "react";
+import classNames from "classnames";
 import foto1 from "../img/mainLyM4.jpg";
 
-function GalleryTwo() 
-{
-    return (<section className="contA">
-                <div className="container">
-                    <div className="row d-flex align-items-center justify-content-center galleryOneContainer">
+let options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.4,
+};
 
-                        <div className="col-lg-6">
-                            <img src={foto1} className="ourPhoto" />
-                        </div>   
-                        <div className="col-lg-6">
-                            <img src={foto1} className="ourPhoto" />
-                        </div>
-                    </div>
-                </div>
+function GalleryTwo() {
+  const ref = useRef(null);
+  const [show, setShow] = useState(false);
+  const callbackFunc = (entries) => {
+    entries.forEach((entry) => {
+      setShow(entry.isIntersecting);
+      if (entry.isIntersecting) {
+      }
+    });
+  };
 
-            </section>);
+  useEffect(() => {
+    const observer = new IntersectionObserver(callbackFunc, options);
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      observer.unobserve(ref.current);
+    };
+  }, []);
+  return (
+    <section className="contA" ref={ref}>
+      <div className="container">
+        <div className="row d-flex align-items-center justify-content-center galleryOneContainer">
+          <div className="col-lg-6">
+            <img
+              src={foto1}
+              className={classNames(
+                "ourPhoto",
+                show ? "showOurPhoto" : "",
+                show ? "delay-01" : ""
+              )}
+            />
+          </div>
+          <div className="col-lg-6">
+            <img
+              src={foto1}
+              className={classNames(
+                "ourPhoto",
+                show ? "showOurPhoto" : "",
+                show ? "delay-02" : ""
+              )}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 export default GalleryTwo;
