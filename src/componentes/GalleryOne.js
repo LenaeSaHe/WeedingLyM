cbedc17cc123249a6a084333ee7e8fa0847ff962
Marcos Rivas <marcosrivasr@gmail.com>
2023-03-26 19:59:22 -0600

    Padding para nuestras imagenes

--- src/componentes/GalleryOne.js
+++ src/componentes/GalleryOne.js
@@ -3,35 +3,9 @@ import classNames from "classnames";
 import foto1 from "../img/mainLyM4.jpg";
 import OurPhoto from "./OurPhoto";
 
-let options = {
-  root: null,
-  rootMargin: "0px",
-  threshold: 0.4,
-};
-
 function GalleryOne() {
-  const ref = useRef(null);
-  const [show, setShow] = useState(false);
-  const callbackFunc = (entries) => {
-    entries.forEach((entry) => {
-      setShow(entry.isIntersecting);
-      if (entry.isIntersecting) {
-      }
-    });
-  };
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(callbackFunc, options);
-    if (ref.current) {
-      observer.observe(ref.current);
-    }
-
-    return () => {
-      observer.unobserve(ref.current);
-    };
-  }, []);
   return (
-    <section className="contA" ref={ref}>
+    <section className="contA" style={{ padding: "20px" }}>
       <div className="row d-flex align-items-center justify-content-center galleryOneContainer">
         <div className="col-lg-4">
           <OurPhoto photo={foto1} delay="1" />
--- src/componentes/GalleryTwo.js
+++ src/componentes/GalleryTwo.js
@@ -3,43 +3,15 @@ import classNames from "classnames";
 import foto1 from "../img/mainLyM4.jpg";
 import OurPhoto from "./OurPhoto";
 
-let options = {
-  root: null,
-  rootMargin: "0px",
-  threshold: 0.4,
-};
-
 function GalleryTwo() {
-  const ref = useRef(null);
-  const [show, setShow] = useState(false);
-  const callbackFunc = (entries) => {
-    entries.forEach((entry) => {
-      setShow(entry.isIntersecting);
-      if (entry.isIntersecting) {
-      }
-    });
-  };
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(callbackFunc, options);
-    if (ref.current) {
-      observer.observe(ref.current);
-    }
-
-    return () => {
-      observer.unobserve(ref.current);
-    };
-  }, []);
   return (
-    <section className="contA" ref={ref}>
-      <div className="container">
-        <div className="row d-flex align-items-center justify-content-center galleryOneContainer">
-          <div className="col-lg-6">
-            <OurPhoto photo={foto1} delay="1" />
-          </div>
-          <div className="col-lg-6">
-            <OurPhoto photo={foto1} delay="2" />
-          </div>
+    <section className="contA" style={{ padding: "20px" }}>
+      <div className="row d-flex align-items-center justify-content-center galleryOneContainer">
+        <div className="col-lg-6">
+          <OurPhoto photo={foto1} delay="1" />
+        </div>
+        <div className="col-lg-6">
+          <OurPhoto photo={foto1} delay="2" />
         </div>
       </div>
     </section>
