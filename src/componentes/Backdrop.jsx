bc11c70c52c3c8724b5d9633670560de1708190f
Marcos Rivas <marcosrivasr@gmail.com>
2023-03-30 21:37:48 -0600

    Pantalla de beinvenida 2.0

--- src/componentes/Backdrop.jsx
+++ src/componentes/Backdrop.jsx
@@ -1,38 +1,29 @@
-import { useEffect, useRef, useReducer, useState } from "react";
-import logo from "../img/L&M.png";
-import loadingGif from "../img/loadingWeeding.gif";
+import { useEffect, useRef, useReducer, useState, forwardRef } from "react";
 import classNames from "classnames";
-
 import { preloadImages } from "../datos/util";
-import mainMyL from "../img/mainLyM.jpg";
-import mainMyL2 from "../img/mainLyM2.jpg";
-import mainMyL3 from "../img/mainLyM3.jpg";
-import mainMyL4 from "../img/mainLyM4.jpg";
+import { default as images } from "../datos/images";
+import rings from "../img/loadingWeeding02.gif";
 
 const reducer = (p) => !p;
 
 function Backdrop({ invitado }) {
-  const loadingRef = useRef(null);
   const progressRef = useRef(0);
   const [, render] = useReducer(reducer, false);
   const [loading, setLoading] = useState(true);
+  const [hideBackdrop, setHideBackdrop] = useState(false);
+  const ref = useRef();
 
   useEffect(function () {
     addScroll();
 
-    const images = [logo, mainMyL, mainMyL2, mainMyL3, mainMyL4];
     preloadImages(
       images,
       () => {
         /* onFinish */
-        if (loadingRef.current) {
-          //loadingRef?.current.classList.add("fadeOut");
-          setTimeout(() => {
-            setLoading(false);
-          }, 500);
-        }
-
-        // console.log("item", averageRef.current);
+        setTimeout(() => {
+          setHideBackdrop(true);
+          setLoading(false);
+        }, 1000);
       },
       (progress) => {
         /* onProgress */
@@ -41,10 +32,9 @@ function Backdrop({ invitado }) {
       }
     );
   }, []);
-  const ref = useRef();
-  {
-    /* obtener la referencia del html*/
-  }
+  /**
+   * Add scroll to body after animation ends
+   */
   function addScroll() {
     ref.current.addEventListener("animationend", function () {
       document.querySelector("body").style.overflow = "auto";
@@ -54,29 +44,50 @@ function Backdrop({ invitado }) {
     <div
       id="backdrop"
       ref={ref}
-      className={!loading ? classNames("backdropAnimation") : null}
+      className={hideBackdrop ? classNames("backdropAnimation") : null}
     >
       <div className="backdropContainer">
-        <div>
-          <span className={classNames("fadeIn delay-5", "greetingBackdrop")}>
-            ¡Hola, {invitado ? invitado.nickname.split(" ")[0] : ""}!
-          </span>
-        </div>
-        <div class="bd-background">
-          <div class="bd-background-layer layer1"></div>
-          <div class="bd-background-layer layer2"></div>
-          <div class="bd-background-layer layer3"></div>
-          <div class="bd-background-layer layer4"></div>
-          <div class="bd-background-layer layer5"></div>
-          <div class="bd-background-layer layer6"></div>
-          <div class="bd-background-layer layer7"></div>
-          <div class="bd-background-layer layer8"></div>
-        </div>
-        <div className={classNames("fadeIn delay-5", "message m-auto mt-3")}>
-          Te invitamos a compartir este bello momento con nosotros
-        </div>
-        <div ref={loadingRef} className="loadingContainer"></div>
-        <div className="messagePercent">{progressRef.current}% ...</div>
+        {loading ? <LoadingScreen ref={progressRef} /> : null}
+        {hideBackdrop ? <WelcomeScreen invitado={invitado} /> : null}
+      </div>
+    </div>
+  );
+}
+
+const LoadingScreen = forwardRef((props, ref) => {
+  return (
+    <>
+      <div className="loadingContainer">
+        <img src={rings} />
+      </div>
+      <div className="messagePercent">
+        <span>Preparando tu invitacion...</span> <br />
+        {ref.current}% lista
+      </div>
+    </>
+  );
+});
+
+function WelcomeScreen({ invitado }) {
+  return (
+    <div>
+      <div>
+        <span className={classNames("fadeIn", "greetingBackdrop")}>
+          ¡Hola, {invitado ? invitado.nickname.split(" ")[0] : ""}!
+        </span>
+      </div>
+      <div className="bd-background">
+        <div className="bd-background-layer layer1"></div>
+        <div className="bd-background-layer layer2"></div>
+        <div className="bd-background-layer layer3"></div>
+        <div className="bd-background-layer layer4"></div>
+        <div className="bd-background-layer layer5"></div>
+        <div className="bd-background-layer layer6"></div>
+        <div className="bd-background-layer layer7"></div>
+        <div className="bd-background-layer layer8"></div>
+      </div>
+      <div className={classNames("fadeIn", "message m-auto mt-3")}>
+        Te invitamos a compartir este bello momento con nosotros
       </div>
     </div>
   );
---
+++ src/datos/images.js
@@ -0,0 +1,7 @@
+import mainMyL from "../img/mainLyM.jpg";
+import mainMyL2 from "../img/mainLyM2.jpg";
+import mainMyL3 from "../img/mainLyM3.jpg";
+import mainMyL4 from "../img/mainLyM4.jpg";
+import logo from "../img/L&M.png";
+
+export default [logo, mainMyL, mainMyL2, mainMyL3, mainMyL4];
