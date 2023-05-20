78daa2cfe233e92c95fa5691d909c0db84b89ae8
Marcos Rivas <marcosrivasr@gmail.com>
2023-05-19 22:31:08 -0600

    fotos papas

--- src/componentes/Family.js
+++ src/componentes/Family.js
@@ -12,11 +12,13 @@ function Family() {
             title="Padres de la novia"
             father="Julio César Sánchez Rivera"
             mother="Maria Elena Hernández Hernández"
+            stylePhoto="photoPapasLena"
           />
           <FamilyItem
             title="Padres del novio"
             father="Marcos Rivas Arzaluz"
             mother="Alejandra Rojas Brito"
+            stylePhoto="photoPapasMarcos"
           />
         </div>
       </div>
